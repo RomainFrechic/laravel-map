e21d790ed1044e93b64a1359b5bb39171c0a3313
Romain <RomainFrechic@outlook.fr>
2017-02-22 13:31:35 +0100

    Générer une Map interactive

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tous les simploniens | Made by Simplon Toulouse</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+	<!--Début liens Leaflet à conserver -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet/1.0.2/leaflet.css"/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet.markercluster/1.0.0/MarkerCluster.css"/>
+	<!--Fin liens Leaflet à conserver -->
+	<link rel="stylesheet" href="{{ mix('/css/app.css' )}}">
+
+</head>
+<body>
+	  <div class="container">
+            @yield('content')
+        </div>
+
+
+<script src="https://cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js"></script>
+<!--Début liens Leaflet à conserver -->
+<script src="https://cdn.jsdelivr.net/leaflet/1.0.2/leaflet.js"></script>
+<script src="https://cdn.jsdelivr.net/leaflet.markercluster/1.0.0/leaflet.markercluster.js"></script>
+<!--Fin liens Leaflet à conserver -->
+<script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/mustache.js/2.3.0/mustache.min.js"></script>
+
+<script type="text/javascript" src="{{ mix('/js/app.js' )}}"></script>
+
+</body>
+</html>
---
+++ resources/views/vue.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+
+
+    <div class="mapContainer" id="partiemap">
+        <div id="map" class="map"></div>
+    </div>
+
+   
+        
+@endsection
